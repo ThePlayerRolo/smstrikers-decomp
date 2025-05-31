31ebe0a35458351f175d9debb711d48693eb692b
yannicksuter <yannick.suter@gmail.com>
2025-05-31 14:04:54 +0200

    clean up

--- src/PowerPC_EABI_Support/Runtime/__init_cpp_exceptions.cpp
+++ src/PowerPC_EABI_Support/Runtime/__init_cpp_exceptions.cpp
@@ -1,30 +1,33 @@
 static int fragmentID = -2;
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-typedef struct __eti_init_info {
-    void* eti_start;
-    void* eti_end;
-    void* code_start;
-    unsigned long code_size;
-} __eti_init_info;
+    typedef struct __eti_init_info
+    {
+        void* eti_start;
+        void* eti_end;
+        void* code_start;
+        unsigned long code_size;
+    } __eti_init_info;
 
-__declspec(section ".init") extern __eti_init_info _eti_init_info[];
+    __declspec(section ".init") extern __eti_init_info _eti_init_info[];
 
-extern void __init_cpp_exceptions(void);
-extern void __fini_cpp_exceptions(void);
-extern void suspend(void);
-extern void __unregister_fragment(int fragmentID);
-extern int __register_fragment(void* _eti_init_info, char* TOC);
-extern void __destroy_global_chain(void);
+    extern void __init_cpp_exceptions(void);
+    extern void __fini_cpp_exceptions(void);
+    extern void suspend(void);
+    extern void __unregister_fragment(int fragmentID);
+    extern int __register_fragment(void* _eti_init_info, char* TOC);
+    extern void __destroy_global_chain(void);
 
 #ifdef __cplusplus
 }
 #endif
 
-asm char* GetR2(void) {
+asm char* GetR2(void)
+{
     // clang-format off
     nofralloc
     mr r3, r2
@@ -32,15 +35,19 @@ asm char* GetR2(void) {
     // clang-format on
 }
 
-extern void __fini_cpp_exceptions(void) {
-    if (fragmentID != -2) {
+extern void __fini_cpp_exceptions(void)
+{
+    if (fragmentID != -2)
+    {
         __unregister_fragment(fragmentID);
         fragmentID = -2;
     }
 }
 
-extern void __init_cpp_exceptions(void) {
-    if (fragmentID == -2) {
+extern void __init_cpp_exceptions(void)
+{
+    if (fragmentID == -2)
+    {
         fragmentID = __register_fragment(&_eti_init_info, GetR2());
     }
 }
