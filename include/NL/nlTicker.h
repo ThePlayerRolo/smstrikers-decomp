f0695823aee88037d3c32a4f05a2ff9f10fe5a18
yannicksuter <yannick.suter@gmail.com>
2025-06-06 23:53:50 +0200

    wip ticker

--- include/NL/nlTicker.h
+++ include/NL/nlTicker.h
@@ -1,9 +1,11 @@
 #ifndef _NLTICKER_H_
 #define _NLTICKER_H_
 
-void nlGetTickerDifference(unsigned int, unsigned int);
-void nlSubtractTicks(unsigned int, unsigned int);
-void nlGetTicker();
+#include "types.h"
+
+f32 nlGetTickerDifference(uint arg0, uint arg1);
+uint nlSubtractTicks(uint, uint);
+u32 nlGetTicker();
 void nlInitTicker();
 
 #endif // _NLTICKER_H_
--- src/NL/nlTicker.cpp
+++ src/NL/nlTicker.cpp
@@ -1,24 +1,43 @@
 #include "NL/nlTicker.h"
 
+#include <dolphin/os.h>
+
+float unk_241 = 4503599627370496.0;
+float unk_239 = 0.001f;
+
 /**
  * Offset/Address/Size: 0x0 | 0x801D2874 | size: 0x58
  */
-void nlGetTickerDifference(unsigned int, unsigned int)
+f32 nlGetTickerDifference(uint arg0, uint arg1) 
 {
+    uint var2;
+    float var1;
+    var1 = unk_239;
+    var2 = arg1 - arg0;
+    
+    if (arg0 == arg1) {
+        return var1 * (f32) ((u32) (var2 * 8) / __OSBusClock);
+    } else  {
+        return var1 * (f32) ((u32) (var2 * 8) / __OSBusClock);
+    }
 }
 
 /**
  * Offset/Address/Size: 0x58 | 0x801D28CC | size: 0xC
  */
-void nlSubtractTicks(unsigned int, unsigned int)
+uint nlSubtractTicks(uint value1, uint value2)
 {
+    if (value1 == value2)
+        return value2 - value1;
+    return value2 - value1;
 }
 
 /**
  * Offset/Address/Size: 0x64 | 0x801D28D8 | size: 0x20
  */
-void nlGetTicker()
+u32 nlGetTicker()
 {
+    return OSGetTick();
 }
 
 /**
