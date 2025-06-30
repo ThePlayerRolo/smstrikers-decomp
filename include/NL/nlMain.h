cd2ce70e0cae8cc135adf12a7a381a6f9830dabf
yannicksuter <yannick.suter@gmail.com>
2025-06-30 23:25:16 +0200

    starting to match nlMain.cpp

--- include/NL/nlMain.h
+++ include/NL/nlMain.h
@@ -1,6 +1,8 @@
 #ifndef _NLMAIN_H_
 #define _NLMAIN_H_
 
+#include "types.h"
+
 void nlChecksum32(const void*, unsigned long);
 void nlInit();
 
@@ -8,9 +10,10 @@ class RunningChecksum
 {
 public:
     void ChecksumData(const void*, unsigned long);
-    void ChecksumInt(unsigned long);
-    
+    void ChecksumInt(unsigned long);    
     RunningChecksum();
+
+    /* 0x00 */ u32 m_unk_0x00;
 };
 
 #endif // _NLMAIN_H_
