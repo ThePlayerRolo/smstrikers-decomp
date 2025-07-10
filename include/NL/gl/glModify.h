fde7337dc480b2650d490d944ac85e6d5a560a75
yannicksuter <yannick.suter@gmail.com>
2025-07-10 18:42:50 +0200

    progress on glModify

--- include/NL/gl/glModify.h
+++ include/NL/gl/glModify.h
@@ -1,11 +1,14 @@
 #ifndef _GLMODIFY_H_
 #define _GLMODIFY_H_
 
-class glModelPacket;
+#include "NL/gl/glUserData.h"
 
 enum eGLModifier
 {
     eGLModifier_0,
+    eGLModifier_1,
+    eGLModifier_2,
+    eGLModifier_3
 };
 
 void gl_ModifyAddMapping(eGLModifier, unsigned long);
@@ -13,5 +16,6 @@ void gl_ModifyAddMapping(eGLModifier, unsigned long, unsigned long);
 void gl_ModifyClearLastMapping();
 void gl_ModifyClearMappings();
 void gl_Modify(const glModelPacket*);
-void gl_ModifyGetNum();
+u32 gl_ModifyGetNum();
+
 #endif // _GLMODIFY_H_
--- src/NL/gl/glModify.cpp
+++ src/NL/gl/glModify.cpp
@@ -1,17 +1,40 @@
 #include "NL/gl/glModify.h"
+#include "NL/gl/glMemory.h"
+#include <string.h>
+
+struct Modifier
+{
+    /* 0x00 */ eGLModifier m_modifier;
+    /* 0x04 */ s32 m_unk_0x04;
+    /* 0x08 */ s32 m_unk_0x08;
+};
+
+Modifier glModifier[6];
+u32 glNumModifiers = 0;
+
+static u32 GLTT_Diffuse_bit = 0x00000001;
+static u32 GLTT_Gloss_bit = 0x00000010;
 
 /**
  * Offset/Address/Size: 0x0 | 0x801D903C | size: 0x30
  */
-void gl_ModifyAddMapping(eGLModifier, unsigned long)
+void gl_ModifyAddMapping(eGLModifier arg0, unsigned long arg1)
 {
+    glModifier[glNumModifiers].m_modifier = arg0;
+    glModifier[glNumModifiers].m_unk_0x04 = -1;
+    glModifier[glNumModifiers].m_unk_0x08 = arg1;
+    glNumModifiers++;
 }
 
 /**
  * Offset/Address/Size: 0x30 | 0x801D906C | size: 0x2C
  */
-void gl_ModifyAddMapping(eGLModifier, unsigned long, unsigned long)
+void gl_ModifyAddMapping(eGLModifier arg0, unsigned long arg1, unsigned long arg2)
 {
+    glModifier[glNumModifiers].m_modifier = arg0;
+    glModifier[glNumModifiers].m_unk_0x04 = arg1;
+    glModifier[glNumModifiers].m_unk_0x08 = arg2;
+    glNumModifiers++;
 }
 
 /**
@@ -19,6 +42,14 @@ void gl_ModifyAddMapping(eGLModifier, unsigned long, unsigned long)
  */
 void gl_ModifyClearLastMapping()
 {
+    s32 temp_r0;
+
+    temp_r0 = glNumModifiers - 1;
+    glNumModifiers = temp_r0;
+    if (temp_r0 < 0)
+    {
+        glNumModifiers = 0;
+    }
 }
 
 /**
@@ -26,18 +57,78 @@ void gl_ModifyClearLastMapping()
  */
 void gl_ModifyClearMappings()
 {
+    glNumModifiers = 0;
 }
 
 /**
  * Offset/Address/Size: 0x84 | 0x801D90C0 | size: 0x1F8
  */
-void gl_Modify(const glModelPacket*)
-{
+void gl_Modify(const glModelPacket* arg0)
+{    
+    glModelPacket* packet = NULL;
+    for (u32 i = 0; i < glNumModifiers; i++)
+    {
+        switch (glModifier[i].m_modifier)
+        { /* irregular */
+        case eGLModifier_0:
+            if (arg0->m_unk_0x1C == glModifier[i].m_unk_0x04)
+            {
+                if (packet == NULL)
+                {
+                    packet = (glModelPacket*)glFrameAlloc(0x4A, eGLMemory_0);
+                    memcpy(packet, arg0->m_unk_0x00, 0x4A);
+                }
+                packet->m_unk_0x1C = glModifier[i].m_unk_0x08;
+            }
+            break;
+        case eGLModifier_1:
+            // u32 temp_r4 = glModifier[i].m_unk_0x04;
+            if (glModifier[i].m_unk_0x04 + 0x10000 == -1U)
+            {
+                if (packet == NULL)
+                {
+                    packet = (glModelPacket*)glFrameAlloc(0x4A, eGLMemory_0);
+                    memcpy(packet, arg0->m_unk_0x00, 0x4A);
+                }
+                packet->m_unk_0x28 = glModifier[i].m_unk_0x08;
+                packet->m_unk_0x40 = packet->m_unk_0x40 | GLTT_Diffuse_bit;
+            }
+            else if (arg0->m_unk_0x28 == glModifier[i].m_unk_0x04)
+            {
+                if (packet == NULL)
+                {
+                    packet = (glModelPacket*)glFrameAlloc(0x4A, eGLMemory_0);
+                    memcpy(packet, arg0->m_unk_0x00, 0x4A);
+                }
+                packet->m_unk_0x28 = glModifier[i].m_unk_0x08;
+                packet->m_unk_0x40 = (u8)(GLTT_Diffuse_bit | packet->m_unk_0x40);
+            }
+            break;
+        case eGLModifier_2:
+            if (packet == NULL)
+            {
+                packet = (glModelPacket*)glFrameAlloc(0x4A, eGLMemory_0);
+                memcpy(packet, arg0->m_unk_0x00, 0x4A);
+            }
+            packet->m_unk_0x38 = glModifier[i].m_unk_0x08;
+            packet->m_unk_0x40 = (u8)(GLTT_Gloss_bit | packet->m_unk_0x40);
+            break;
+        case eGLModifier_3:
+            if (packet == NULL)
+            {
+                packet = (glModelPacket*)glFrameAlloc(0x4A, eGLMemory_0);
+                memcpy(packet, arg0->m_unk_0x00, 0x4A);
+            }
+            packet->m_unk_0x40 = (u8)(packet->m_unk_0x40 & glModifier[i].m_unk_0x08);
+            break;
+        }
+    }
 }
 
 /**
  * Offset/Address/Size: 0x27C | 0x801D92B8 | size: 0x8
  */
-void gl_ModifyGetNum()
+u32 gl_ModifyGetNum()
 {
+    return glNumModifiers;
 }
