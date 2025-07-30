1e790a25a274fec61f9e3cef2f34d269dc8abed0
yannicksuter <yannick.suter@gmail.com>
2025-07-30 21:22:24 +0200

    progress TextureAnim

--- src/Game/GL/GLTextureAnim.cpp
+++ src/Game/GL/GLTextureAnim.cpp
@@ -1,45 +1,220 @@
 #include "GLTextureAnim.h"
+#include "NL/nlMemory.h"
 
 /**
  * Offset/Address/Size: 0x0 | 0x801E7988 | size: 0x120
  */
-void GLTextureAnim::Update(float)
+void GLTextureAnim::Update(float deltaTime)
 {
+    // Early return if animation is stopped
+    if (m_isStopped)
+    {
+        return;
+    }
+
+    // Early return if we have less than 2 textures
+    if (m_numTextures < 2)
+    {
+        return;
+    }
+
+    // Update current time
+    m_currentTime += deltaTime;
+
+    // Get the time threshold for the current frame
+    u32 frameIndex = m_currentFrame;
+    GLAnimTex* textureArray = m_textureArray;
+    u32 offset = frameIndex * 8 + 4; // 8 bytes per GLAnimTex, +4 for time field
+    f32 timeThreshold = *(f32*)((u8*)textureArray + offset);
+
+    // Check if we've reached the time threshold for the current frame
+    if (m_currentTime >= timeThreshold)
+    {
+        // Reset current time
+        m_currentTime = 0.0f;
+
+        // Handle different animation types
+        switch (m_animationType)
+        {
+            case 0: // Loop
+                m_currentFrame++;
+                if (m_currentFrame >= m_numTextures)
+                {
+                    m_currentFrame = 0;
+                }
+                break;
+
+            case 1: // Ping-pong
+                if (m_direction > 0)
+                {
+                    m_currentFrame++;
+                    if (m_currentFrame >= m_numTextures)
+                    {
+                        m_currentFrame = m_numTextures - 2;
+                        m_direction = -1;
+                    }
+                }
+                else
+                {
+                    m_currentFrame--;
+                    if (m_currentFrame < 0)
+                    {
+                        m_currentFrame = 1;
+                        m_direction = 1;
+                    }
+                }
+                break;
+
+            case 2: // Once
+                m_currentFrame++;
+                if (m_currentFrame >= m_numTextures)
+                {
+                    m_currentFrame = m_numTextures - 1;
+                }
+                break;
+
+            case 3: // Reverse
+                m_currentFrame++;
+                if (m_currentFrame >= m_numTextures)
+                {
+                    m_currentFrame = m_numTextures - 1;
+                }
+                break;
+        }
+    }
 }
 
 /**
  * Offset/Address/Size: 0x120 | 0x801E7AA8 | size: 0xA0
  */
-void GLTextureAnim::GetTextureHandle(float)
+u32 GLTextureAnim::GetTextureHandle(float time)
 {
+    GLAnimTex* textureArray = m_textureArray;
+    u32 frameIndex = 0;
+
+    while (true)
+    {
+        u32 actualFrame;
+        if (frameIndex < 0)
+        {
+            actualFrame = m_currentFrame;
+        }
+        else
+        {
+            actualFrame = frameIndex;
+        }
+
+        u32 offset = actualFrame * 8;
+        GLAnimTex* currentTex = (GLAnimTex*)((u8*)textureArray + offset);
+        f32 timeThreshold = currentTex->m_time;
+        u32 textureHandle = currentTex->m_textureHandle;
+
+        // Check if we've found the right time slot
+        if (time < timeThreshold)
+        {
+            return textureHandle;
+        }
+
+        // Handle different animation types for frame progression
+        switch (m_animationType)
+        {
+            case 0: // Loop
+                frameIndex++;
+                if (frameIndex >= m_numTextures)
+                {
+                    frameIndex = 0;
+                }
+                break;
+
+            case 1: // Ping-pong
+                frameIndex++;
+                if (frameIndex >= m_numTextures)
+                {
+                    frameIndex = 0;
+                }
+                break;
+
+            case 2: // Once
+                frameIndex++;
+                if (frameIndex >= m_numTextures)
+                {
+                    return textureHandle; // Return last texture
+                }
+                break;
+
+            case 3: // Reverse
+                frameIndex++;
+                if (frameIndex >= m_numTextures)
+                {
+                    return textureHandle; // Return last texture
+                }
+                break;
+        }
+    }
 }
 
 /**
  * Offset/Address/Size: 0x1C0 | 0x801E7B48 | size: 0x1C
  */
-void GLTextureAnim::GetTexture(int)
+GLAnimTex* GLTextureAnim::GetTexture(int frameIndex)
 {
+    GLAnimTex* textureArray = m_textureArray;
+    
+    // If frameIndex is negative, use current frame
+    if (frameIndex < 0)
+    {
+        frameIndex = m_currentFrame;
+    }
+
+    u32 offset = frameIndex * 8;
+    return (GLAnimTex*)((u8*)textureArray + offset);
 }
 
 /**
  * Offset/Address/Size: 0x1DC | 0x801E7B64 | size: 0x20
  */
-void GLTextureAnim::SetTexture(int, const GLAnimTex&)
+void GLTextureAnim::SetTexture(int frameIndex, const GLAnimTex& animTex)
 {
+    GLAnimTex* textureArray = m_textureArray;
+    u32 offset = frameIndex * 8;
+    GLAnimTex* targetTex = (GLAnimTex*)((u8*)textureArray + offset);
+    
+    targetTex->m_textureHandle = animTex.m_textureHandle;
+    targetTex->m_time = animTex.m_time;
 }
 
 /**
  * Offset/Address/Size: 0x1FC | 0x801E7B84 | size: 0x18
  */
-void GLTextureAnim::SetFrame(int)
+void GLTextureAnim::SetFrame(int frameIndex)
 {
+    // Calculate frame within bounds using modulo
+    u32 numTextures = m_numTextures;
+    u32 frame = frameIndex / numTextures;
+    frame = frame * numTextures;
+    frame = frameIndex - frame;
+    m_currentFrame = frame;
 }
 
 /**
  * Offset/Address/Size: 0x214 | 0x801E7B9C | size: 0x74
  */
-void GLTextureAnim::SetNumTextures(int)
+void GLTextureAnim::SetNumTextures(int numTextures)
 {
+    // Free existing texture array if it exists
+    if (m_textureArray != nullptr)
+    {
+        delete [] m_textureArray;
+    }
+
+    // Allocate new texture array
+    u32 size = numTextures * 8; // 8 bytes per GLAnimTex
+    m_textureArray = (GLAnimTex*)nlMalloc(size, 8, 0);
+    
+    m_numTextures = numTextures;
+    m_direction = 1;
+    m_currentFrame = 0;
+    m_currentTime = 0.0f;
 }
 
 /**
@@ -47,6 +222,11 @@ void GLTextureAnim::SetNumTextures(int)
  */
 GLTextureAnim::~GLTextureAnim()
 {
+    // Free texture array if it exists
+    if (m_textureArray != nullptr)
+    {
+        delete [] m_textureArray;
+    }
 }
 
 /**
@@ -55,10 +235,10 @@ GLTextureAnim::~GLTextureAnim()
 GLTextureAnim::GLTextureAnim()
 {
     m_unk_0x00 = -1;
-    m_unk_0x04 = 0;
-    m_unk_0x08 = 0;
-    m_unk_0x0C = 1;
-    m_unk_0x14 = 0;
-    m_unk_0x1C = 0;
-    m_unk_0x10 = false;
+    m_numTextures = 0;
+    m_animationType = 0;
+    m_direction = 1;
+    m_currentFrame = 0;
+    m_textureArray = nullptr;
+    m_isStopped = false;
 }
--- src/Game/GL/GLTextureAnim.h
+++ src/Game/GL/GLTextureAnim.h
@@ -3,29 +3,33 @@
 
 #include "types.h"
 
-class GLAnimTex;
+struct GLAnimTex
+{
+    /* 0x00 */ u32 m_textureHandle;
+    /* 0x04 */ f32 m_time;
+};
 
 class GLTextureAnim
 {
 public:
-    void Update(float);
-    void GetTextureHandle(float);
-    void GetTexture(int);
-    void SetTexture(int, const GLAnimTex&);
-    void SetFrame(int);
-    void SetNumTextures(int);
+    void Update(float deltaTime);
+    u32 GetTextureHandle(float time);
+    GLAnimTex* GetTexture(int frameIndex);
+    void SetTexture(int frameIndex, const GLAnimTex& animTex);
+    void SetFrame(int frameIndex);
+    void SetNumTextures(int numTextures);
 
     ~GLTextureAnim();
     GLTextureAnim();
 
     /* 0x00 */ s32 m_unk_0x00;
-    /* 0x04 */ u32 m_unk_0x04;
-    /* 0x08 */ u32 m_unk_0x08;
-    /* 0x0C */ u32 m_unk_0x0C;
-    /* 0x10 */ bool m_unk_0x10;
-    /* 0x14 */ u32 m_unk_0x14;
-    /* 0x18 */ u32 m_unk_0x18_unused;
-    /* 0x14 */ u32 m_unk_0x1C;
+    /* 0x04 */ u32 m_numTextures;
+    /* 0x08 */ u32 m_animationType; // 0=loop, 1=ping-pong, 2=once, 3=reverse
+    /* 0x0C */ s32 m_direction; // 1=forward, -1=backward (for ping-pong)
+    /* 0x10 */ bool m_isStopped;
+    /* 0x14 */ u32 m_currentFrame;
+    /* 0x18 */ f32 m_currentTime;
+    /* 0x1C */ GLAnimTex* m_textureArray;
 };
 
 #endif // _GLTEXTUREANIM_H_
