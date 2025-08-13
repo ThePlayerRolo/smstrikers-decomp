51ea73ea76b8cb70247106162ec2dd70027d0078
Yannick Suter <yannick.suter@gmail.com>
2025-08-13 15:04:09 +0200

    matched ClockManager::Update with help of ribbanya

--- src/Game/clock.cpp
+++ src/Game/clock.cpp
@@ -27,15 +27,19 @@ void Clock::Stop()
  */
 void Clock::Start()
 {
-    if (m_unk_0x18 != 1) {
+    if (m_unk_0x18 != 1)
+    {
         nlDLRingRemove<Clock>(&ClockManager::m_inactiveList, this);
-        if (ClockManager::m_bUpdatingClocks != 0) {
+        if (ClockManager::m_bUpdatingClocks != 0)
+        {
             nlDLRingAddEnd<Clock>(&ClockManager::m_pendingActiveList, this);
-        } else {
+        }
+        else
+        {
             nlDLRingAddEnd<Clock>(&ClockManager::m_activeList, this);
         }
     }
-    m_unk_0x18 = 1;    
+    m_unk_0x18 = 1;
 }
 
 /**
@@ -47,15 +51,15 @@ void Clock::Reset(float param_1, float param_2, float param_3)
     m_unk_0x08 = param_1;
     m_unk_0x0C = param_2;
 
-    if (m_unk_0x18 == 1) 
+    if (m_unk_0x18 == 1)
     {
-        if (nlDLRingRemoveSafely<Clock>(&ClockManager::m_activeList, this) == 0) 
+        if (nlDLRingRemoveSafely<Clock>(&ClockManager::m_activeList, this) == 0)
         {
             nlDLRingRemoveSafely<Clock>(&ClockManager::m_pendingActiveList, this);
         }
         nlDLRingAddEnd<Clock>(&ClockManager::m_inactiveList, this);
     }
-    m_unk_0x18 = 0;    
+    m_unk_0x18 = 0;
 }
 
 /**
@@ -91,54 +95,51 @@ void ClockManager::Update(float arg8)
 {
     ClockCallback temp_r12;
     ClockCallback temp_r12_2;
-   
-    Clock *var_r31;
-    Clock *temp_r30;
 
-    if (m_activeList != NULL) 
+    Clock* var_r31;
+    Clock* temp_r30;
+
+    if (m_activeList != NULL)
     {
         m_bUpdatingClocks = true;
         var_r31 = m_activeList->m_next;
 
-loop_2:
+    loop_2:
         temp_r30 = var_r31->m_next;
-        if (((s32) var_r31->m_unk_0x18 == 3) || !(var_r31->m_unk_0x14 & nlTaskManager::m_pInstance->m_unk_0x08)) 
+        if (((s32)var_r31->m_unk_0x18 == 3) || !(var_r31->m_unk_0x14 & nlTaskManager::m_pInstance->m_unk_0x08))
         {
-            if (var_r31 != m_activeList) 
+            if (var_r31 != m_activeList)
             {
                 var_r31 = temp_r30;
                 goto loop_2;
             }
-        } 
-        else 
+        }
+        else
         {
-            var_r31->m_unk_0x08 = (f32) ((arg8 * var_r31->m_unk_0x04) + var_r31->m_unk_0x08);
-            // M2C_ERROR(/* unknown instruction: cror eq, gt, eq */);
-            if (var_r31->m_unk_0x04 >= 0.f) 
+            var_r31->m_unk_0x08 = (f32)((arg8 * var_r31->m_unk_0x04) + var_r31->m_unk_0x08);
+            if (var_r31->m_unk_0x04 >= 0.f)
             {
-                // M2C_ERROR(/* unknown instruction: cror eq, gt, eq */);
-                if (var_r31->m_unk_0x08 >= var_r31->m_unk_0x0C) 
+                if (var_r31->m_unk_0x08 >= var_r31->m_unk_0x0C)
                 {
                     var_r31->m_unk_0x18 = 2;
-                    var_r31->m_unk_0x08 = (f32) var_r31->m_unk_0x0C;
+                    var_r31->m_unk_0x08 = (f32)var_r31->m_unk_0x0C;
                     temp_r12 = var_r31->m_callback;
-                    if (temp_r12 != NULL) 
+                    if (temp_r12 != NULL)
                     {
                         temp_r12(var_r31->m_unk_0x1C, var_r31->m_unk_0x20);
                     }
                     nlDLRingRemove<Clock>(&m_activeList, var_r31);
                     nlDLRingAddEnd<Clock>(&m_inactiveList, var_r31);
                 }
-            } 
-            else 
+            }
+            else
             {
-                // M2C_ERROR(/* unknown instruction: cror eq, lt, eq */);
-                if (var_r31->m_unk_0x08 >= var_r31->m_unk_0x0C) 
+                if (!!(var_r31->m_unk_0x08 <= var_r31->m_unk_0x0C))
                 {
                     var_r31->m_unk_0x18 = 2;
-                    var_r31->m_unk_0x08 = (f32) var_r31->m_unk_0x0C;
+                    var_r31->m_unk_0x08 = (f32)var_r31->m_unk_0x0C;
                     temp_r12_2 = var_r31->m_callback;
-                    if (temp_r12_2 != NULL) 
+                    if (temp_r12_2 != NULL)
                     {
                         temp_r12_2(var_r31->m_unk_0x1C, var_r31->m_unk_0x20);
                     }
@@ -146,8 +147,8 @@ loop_2:
                     nlDLRingAddEnd<Clock>(&m_inactiveList, var_r31);
                 }
             }
-            
-            if ((var_r31 != m_activeList) && (m_activeList != NULL)) 
+
+            if ((var_r31 != m_activeList) && (m_activeList != NULL))
             {
                 var_r31 = temp_r30;
                 goto loop_2;
@@ -156,7 +157,7 @@ loop_2:
         m_bUpdatingClocks = 0;
         nlDLRingAppendRing<Clock>(&m_activeList, m_pendingActiveList);
         m_pendingActiveList = 0;
-    }    
+    }
 }
 
 /**
