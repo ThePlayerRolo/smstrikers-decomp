3c1d3c4deb75165d3a79b5843211857c194c9725
yannicksuter <yannick.suter@gmail.com>
2025-08-18 08:07:25 +0200

    fixed broken build

--- src/Game/FE/FEAudio.cpp
+++ src/Game/FE/FEAudio.cpp
@@ -1,4 +1,6 @@
-#include "FEAudio.h"
+#include "Game/FE/FEAudio.h"
+
+class Event;
 
 /**
  * Offset/Address/Size: 0x0 | 0x8009EDAC | size: 0x8
@@ -56,79 +58,85 @@ void FEAudio::BuildAnimAudioEventLookup()
 {
 }
 
-/**
- * Offset/Address/Size: 0x0 | 0x8009F9A8 | size: 0x10
- */
-void ListContainerBase<AnimAudioEventLookup, BasicSlotPoolHigh<ListEntry<AnimAudioEventLookup>>>::DeleteEntry(ListEntry<AnimAudioEventLookup>*)
-{
-}
+// /**
+//  * Offset/Address/Size: 0x0 | 0x8009F9A8 | size: 0x10
+//  */
+// void ListContainerBase<AnimAudioEventLookup, BasicSlotPoolHigh<ListEntry<AnimAudioEventLookup>>>::DeleteEntry(
+//     ListEntry<AnimAudioEventLookup>*)
+// {
+// }
 
-/**
- * Offset/Address/Size: 0x0 | 0x8009F9B8 | size: 0x20
- */
-void BasicSlotPoolHigh<ListEntry<AnimAudioEventLookup>>::freeFN(void*)
-{
-}
+// /**
+//  * Offset/Address/Size: 0x0 | 0x8009F9B8 | size: 0x20
+//  */
+// void BasicSlotPoolHigh<ListEntry<AnimAudioEventLookup>>::freeFN(void*)
+// {
+// }
 
-/**
- * Offset/Address/Size: 0x20 | 0x8009F9D8 | size: 0x28
- */
-void BasicSlotPoolHigh<ListEntry<AnimAudioEventLookup>>::allocFN(unsigned long)
-{
-}
+// /**
+//  * Offset/Address/Size: 0x20 | 0x8009F9D8 | size: 0x28
+//  */
+// void BasicSlotPoolHigh<ListEntry<AnimAudioEventLookup>>::allocFN(unsigned long)
+// {
+// }
 
-/**
- * Offset/Address/Size: 0x0 | 0x8009FA00 | size: 0x28
- */
-void nlQSort<AnimAudioEventLookup>(AnimAudioEventLookup*, int, int (*)(const AnimAudioEventLookup*, const AnimAudioEventLookup*))
-{
-}
+// /**
+//  * Offset/Address/Size: 0x0 | 0x8009FA00 | size: 0x28
+//  */
+// void nlQSort<AnimAudioEventLookup>(AnimAudioEventLookup*, int, int (*)(const AnimAudioEventLookup*, const AnimAudioEventLookup*))
+// {
+// }
 
-/**
- * Offset/Address/Size: 0x28 | 0x8009FA28 | size: 0x2C
- */
-void nlDefaultQSortComparer<AnimAudioEventLookup>(const AnimAudioEventLookup*, const AnimAudioEventLookup*)
-{
-}
+// /**
+//  * Offset/Address/Size: 0x28 | 0x8009FA28 | size: 0x2C
+//  */
+// void nlDefaultQSortComparer<AnimAudioEventLookup>(const AnimAudioEventLookup*, const AnimAudioEventLookup*)
+// {
+// }
 
-/**
- * Offset/Address/Size: 0x54 | 0x8009FA54 | size: 0x8C
- */
-void nlBSearch<AnimAudioEventLookup, unsigned long>(const unsigned long&, AnimAudioEventLookup*, int)
-{
-}
+// /**
+//  * Offset/Address/Size: 0x54 | 0x8009FA54 | size: 0x8C
+//  */
+// void nlBSearch<AnimAudioEventLookup, unsigned long>(const unsigned long&, AnimAudioEventLookup*, int)
+// {
+// }
 
-/**
- * Offset/Address/Size: 0x0 | 0x8009FAE0 | size: 0x68
- */
-void nlWalkList<ListEntry<AnimAudioEventLookup>, ListContainerBase<AnimAudioEventLookup, BasicSlotPoolHigh<ListEntry<AnimAudioEventLookup>>>>(ListEntry<AnimAudioEventLookup>*, ListContainerBase<AnimAudioEventLookup, BasicSlotPoolHigh<ListEntry<AnimAudioEventLookup>>>*, void (ListContainerBase<AnimAudioEventLookup, BasicSlotPoolHigh<ListEntry<AnimAudioEventLookup>>>::*)(ListEntry<AnimAudioEventLookup>*))
-{
-}
+// /**
+//  * Offset/Address/Size: 0x0 | 0x8009FAE0 | size: 0x68
+//  */
+// void nlWalkList<ListEntry<AnimAudioEventLookup>,
+//                 ListContainerBase<AnimAudioEventLookup, BasicSlotPoolHigh<ListEntry<AnimAudioEventLookup>>>>(
+//     ListEntry<AnimAudioEventLookup>*, ListContainerBase<AnimAudioEventLookup, BasicSlotPoolHigh<ListEntry<AnimAudioEventLookup>>>*,
+//     void (ListContainerBase<AnimAudioEventLookup,
+//     BasicSlotPoolHigh<ListEntry<AnimAudioEventLookup>>>::*)(ListEntry<AnimAudioEventLookup>*))
+// {
+// }
 
-/**
- * Offset/Address/Size: 0x68 | 0x8009FB48 | size: 0x2C
- */
-void nlListAddEnd<ListEntry<AnimAudioEventLookup>>(ListEntry<AnimAudioEventLookup>**, ListEntry<AnimAudioEventLookup>**, ListEntry<AnimAudioEventLookup>*)
-{
-}
+// /**
+//  * Offset/Address/Size: 0x68 | 0x8009FB48 | size: 0x2C
+//  */
+// void nlListAddEnd<ListEntry<AnimAudioEventLookup>>(ListEntry<AnimAudioEventLookup>**, ListEntry<AnimAudioEventLookup>**,
+//                                                    ListEntry<AnimAudioEventLookup>*)
+// {
+// }
 
-/**
- * Offset/Address/Size: 0x0 | 0x8009FB74 | size: 0x84
- */
-void nlStrICmp<char>(const char*, const char*)
-{
-}
+// /**
+//  * Offset/Address/Size: 0x0 | 0x8009FB74 | size: 0x84
+//  */
+// void nlStrICmp<char>(const char*, const char*)
+// {
+// }
 
-/**
- * Offset/Address/Size: 0x84 | 0x8009FBF8 | size: 0x40
- */
-void nlStrCmp<char>(const char*, const char*)
-{
-}
+// /**
+//  * Offset/Address/Size: 0x84 | 0x8009FBF8 | size: 0x40
+//  */
+// void nlStrCmp<char>(const char*, const char*)
+// {
+// }
 
-/**
- * Offset/Address/Size: 0xC4 | 0x8009FC38 | size: 0x20
- */
-void nlToUpper<char>(char)
-{
-}
+// /**
+//  * Offset/Address/Size: 0xC4 | 0x8009FC38 | size: 0x20
+//  */
+// void nlToUpper<char>(char)
+// {
+// }
